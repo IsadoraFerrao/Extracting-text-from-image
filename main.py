336b82418f08b827501a03d527e52b15778d567c
Isadora <isadora-gf@hotmail.com>
2022-10-12 17:23:42 -0300

    listen part

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pytesseract
 import cv2
+import gtts
+from playsound import playsound
 
 # Lendo a imagem
 imagem = cv2.imread("livro.jpg")
 caminho = r"C:\Program Files\Tesseract-OCR"
-# Extraindo texto da imagem
 
+
+# Extraindo texto da imagem
 pytesseract.pytesseract.tesseract_cmd = caminho + r"\tesseract.exe"
 texto = pytesseract.image_to_string(imagem, lang="por") #definindo a linguagem
 print(texto)
 
+frase = gtts.gTTS(texto,lang='pt-br')
+frase.save('frase.mp3')
+playsound('frase.mp3')
